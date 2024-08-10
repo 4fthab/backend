9c1be2b3f9bf52f565e00cb77bdd6b6d58657626
Afthab A <afthabathz@gmail.com>
2024-08-10 11:24:51 +0530

    host removed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware-------------------------
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: "include" }));
 app.use(cookieParser());
 config();
 
